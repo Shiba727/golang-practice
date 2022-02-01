f0e7bae1dd69828b2375d6deea6b232dd3c70850
shiba727 <a6513512@gmail.com>
2022-02-01 23:11:21 +0800

    add banner and access cockroachDB

--- urlshortner/main/main.go
+++ urlshortner/main/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"os"
 	urlshort "urlshortner"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -30,16 +33,61 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	db, err := initStore()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
 	fmt.Printf("Starting the server on :%s", listenAddr)
 	http.ListenAndServe(":"+listenAddr, yamlHandler)
 }
 
 func defaultMux() *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", hello)
+	mux.HandleFunc("/", banner)
 	return mux
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome to the URL shortner!")
+func banner(w http.ResponseWriter, r *http.Request) {
+	banner := `
+	 _   _ ____  _           _                _
+	| | | |  _ \| |      ___| |__   ___  _ __| |_ _ __   ___ _ __
+	| | | | |_) | |     / __| '_ \ / _ \| '__| __| '_ \ / _ \ '__|
+	| |_| |  _ <| |___  \__ \ | | | (_) | |  | |_| | | |  __/ |
+	 \___/|_| \_\_____| |___/_| |_|\___/|_|   \__|_| |_|\___|_|
+	`
+	fmt.Fprintln(w, banner)
+}
+
+func initStore() (*sql.DB, error) {
+	pgConnString := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable",
+		os.Getenv("PGHOST"),
+		os.Getenv("PGPORT"),
+		os.Getenv("PGDATABASE"),
+		os.Getenv("PGUSER"),
+		os.Getenv("PGPASSWORD"),
+	)
+
+	var (
+		db  *sql.DB
+		err error
+	)
+
+	db, err = sql.Open("postgres", pgConnString)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		panic(err)
+	}
+
+	// if _, err := db.Exec(
+	// 	"CREATE TABLE IF NOT EXISTS message (value STRING PRIMARY KEY)"); err != nil {
+	// 	return nil, err
+	// }
+	fmt.Println("Connected to DB.")
+	return db, nil
 }
